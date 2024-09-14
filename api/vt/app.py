3f69139547a36a73a359d41005ed81f378925495
ikoide <ian.koide@protonmail.com>
2024-09-13 21:01:08 -0400

    base app

---
+++ api/vt/app.py
@@ -0,0 +1,10 @@
+import os
+
+from flask import Flask
+
+
+def create_app(config_filename="flask.cfg"):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile(config_filename)
+
+    return app
