626e746519f4adc5e130c3637e3de16c654ac7b9
ikoide <ian.koide@protonmail.com>
2024-09-13 21:21:54 -0400

    base docker setup

--- api/vt/app.py
+++ api/vt/app.py
@@ -1,10 +1,26 @@
 import os
 
 from flask import Flask
+from werkzeug.exceptions import HTTPVersionNotSupported
+
+from mongoengine import connect
 
 
 def create_app(config_filename="flask.cfg"):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile(config_filename)
 
+    initialize_database(app)
+
     return app
+
+
+def initialize_database(app):
+    connect(
+        host="mongodb://"
+        + app.config["MONGODB_HOST"]
+        + ":"
+        + app.config["MONGODB_PORT"]
+        + "/"
+        + app.config["MONGODB_DB"]
+    )
