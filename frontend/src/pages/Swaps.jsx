90dcd1b5d7e5f9d5e83016aaa22ee419c3ba6cc3
Imran Salikhov <imrsalikhov@gmail.com>
2024-09-15 03:19:09 -0400

    dynamic styling

--- frontend/src/pages/Swaps.jsx
+++ frontend/src/pages/Swaps.jsx
@@ -19,11 +19,11 @@ export const Swaps = () => {
           'Accept': 'application/json',
         },
       });
-  
+
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-  
+
       const data = await response.json();
       console.log('Swaps data:', data);
       setSwaps(data); // Store fetched data in state
@@ -43,9 +43,9 @@ export const Swaps = () => {
         display: 'flex',
         flexDirection: 'column', // Stacking cards vertically
         alignItems: 'center', // Centering the cards horizontally
-        }}>
-        
-        <Typography sx={{color: "white", fontWeight: 600, pb: 3}} variant='h3'>Hello, Imran 🫣</Typography>
+      }}>
+
+        <Typography sx={{ color: "white", fontWeight: 600, pb: 3 }} variant='h3'>Hello, Imran 🫣</Typography>
 
         {swaps.length > 0 ? (
           swaps.map((swap, index) => {
@@ -54,45 +54,50 @@ export const Swaps = () => {
             const crn2 = swap.items[1]
             const courseId2 = `${courses[crn2]["Course"].split("-").join(" ")}`
             const status = swap.status
-            return(
-            <Box
-              key={index}
-              sx={{
-                width: 600,
-                padding: "0.5rem"
-              }}
-            >
-              <Card variant="elevation">
-                <CardContent>
-                  <Box sx={{
+
+            const statusStyles = {
+              completed: {
+                backgroundColor: "rgba(18,189,24,0.05)",
+                color: "rgba(18,189,24,0.5)",
+                border: "1px solid rgba(18,189,24,0.2)"
+              }, pending: {
+                backgroundColor: "rgba(229,199,30,0.05)",
+                color: "rgba(229,199,30,0.5)",
+                border: "1px solid rgba(229,199,30,0.2)"
+              }
+            }
+            return (
+              <Box
+                key={index}
+                sx={{
+                  width: 600,
+                  padding: "0.5rem"
+                }}
+              >
+                <Card variant="elevation">
+                  <CardContent>
+                    <Box sx={{
                       display: "flex",
                       justifyContent: "space-between",
                       alignItems: "center"
                     }}>
-                  <Box>
-                  <Typography variant="body2" color='text.secondary'>
-                    SWAP: {swap._id["$oid"]} {/* Assuming `id` is part of the object */}
-                  </Typography>
-                  <Typography variant="h6" color="text.primary" sx={{mb: -1}}>
-                    {courseId1} {`→`} {courseId2} {/* Assuming these are fields */}
-                  </Typography>
-                  </Box>
-                  <Box>
-                    <Button sx={{
-                       // success colors
-                       // backgroundColor: "rgba(18,189,24,0.05)",
-                       // color: "rgba(18,189,24,0.5)",
-                       // border: "1px solid rgba(18,189,24,0.2)"
-                       backgroundColor: "rgba(229,199,30,0.05)",
-                       color: "rgba(229,199,30,0.5)",
-                       border: "1px solid rgba(229,199,30,0.2)"
-                        }}>{status}</Button>
-                  </Box>
-                  </Box>
-                </CardContent>
-              </Card>
-            </Box>
-          )})
+                      <Box>
+                        <Typography variant="body2" color='text.secondary'>
+                          SWAP: {swap._id["$oid"]} {/* Assuming `id` is part of the object */}
+                        </Typography>
+                        <Typography variant="h6" color="text.primary" sx={{ mb: -1 }}>
+                          {courseId1} {`→`} {courseId2} {/* Assuming these are fields */}
+                        </Typography>
+                      </Box>
+                      <Box>
+                        <Button sx={{ ...statusStyles[status] }}>{status}</Button>
+                      </Box>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Box>
+            )
+          })
         ) : (
           <Typography variant="h6" color="text.secondary">
             No swaps available
